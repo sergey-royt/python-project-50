f0e3b8e71215a9f2cc8074b2aa2b2247fadbe529
Sergey R <topgoods687@gmail.com>
2023-08-15 19:17:35 +0300

    add changes

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,20 @@
+import sys
+
+def main():
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-h':
+            print('''gendiff -h
+usage: gendiff [-h] first_file second_file
+
+Compares two configuration files and shows a difference.
+
+positional arguments:
+  first_file
+  second_file
+
+optional arguments:
+  -h, --help            show this help message and exit''')
+
+
+if __name__ == '__main__':
+    main()
