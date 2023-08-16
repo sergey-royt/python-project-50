c345360711b9dc41aa6e3114d53cde57d5d45ba1
Sergey R <topgoods687@gmail.com>
2023-08-16 13:30:17 +0300

    new

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,21 +1,10 @@
-import sys
+import argparse
 
 def main():
-    if len(sys.argv) > 1:
-        if sys.argv[1] == '-h':
-            print('''gendiff -h
-usage: gendiff [-h] first_file second_file
-
-Compares two configuration files and shows a difference.
-
-positional arguments:
-  first_file
-  second_file
-
-optional arguments:
-  -h, --help            show this help message and exit
-  -f FORMAT, --format FORMAT
-                        set format of output''')
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file', type=argparse.FileType('w'))
+    parser.add_argument('second_file', type=argparse.FileType('w'))
+    args = parser.parse_args()
 
 
 if __name__ == '__main__':
