e44a93d6b26a4113179fe3d1b120b8eb836bb0de
Sergey R <topgoods687@gmail.com>
2023-09-12 20:40:45 +0300

    fixing test_gendiff

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,5 +1,5 @@
 import pytest
-from gendiff.gendiff import make_diff
+from gendiff.gendiff import generate_diff
 from tests.fixtures.expected_result import PLAIN_DIFF_1_2, PLAIN_DIFF_2_1
 from tests.fixtures.expected_result import NESTED_STYLISH_DIFF_STR_12
 from tests.fixtures.expected_result import PLAIN_NESTED_DIFF_1_2
@@ -19,4 +19,4 @@ from tests.fixtures.expected_result import PLAIN_NESTED_DIFF_1_2
                          ]
                          )
 def test_gendiff(make_diff_args, expected):
-    assert make_diff(*make_diff_args) == expected
+    assert generate_diff(*make_diff_args) == expected
