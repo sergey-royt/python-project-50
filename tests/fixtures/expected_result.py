a316603afd1ba05240d68622c43f9b097498a346
Sergey R <topgoods687@gmail.com>
2023-09-12 21:15:11 +0300

    fixing tests

--- tests/fixtures/expected_result.py
+++ tests/fixtures/expected_result.py
@@ -29,7 +29,7 @@ NESTED_STYLISH_DIFF_STR_12 = '''{
         }
         setting6: {
             doge: {
-              - wow:
+              - wow: much
               + wow: so much
             }
             key: value
@@ -66,7 +66,7 @@ Property 'common.setting2' was removed
 Property 'common.setting3' was updated. From true to null
 Property 'common.setting4' was added with value: 'blah blah'
 Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
+Property 'common.setting6.doge.wow' was updated. From 'much' to 'so much'
 Property 'common.setting6.ops' was added with value: 'vops'
 Property 'group1.baz' was updated. From 'bas' to 'bars'
 Property 'group1.nest' was updated. From [complex value] to 'str'
