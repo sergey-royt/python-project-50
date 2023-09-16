e0c17d67dcf1d2205a55851b800477b387bc6035
Sergey R <topgoods687@gmail.com>
2023-09-16 19:31:05 +0300

    refactoring input parser

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,13 +1,20 @@
-from gendiff.input_parser import open_file
+import pathlib
+from gendiff.input_parser import parse
 from gendiff.ast_maker import make_ast
 from gendiff.formatters.stylish import gen_stylish_diff
 from gendiff.formatters.plain import gen_plain_diff
 from gendiff.formatters.json import gen_json_diff
 
 
+def get_file_data(file):
+    extension = pathlib.Path(file).suffix
+    content = open(file, 'r')
+    return parse(content, extension)
+
+
 def generate_diff(first_file, second_file, format_type='stylish'):
-    old = open_file(first_file)
-    new = open_file(second_file)
+    old = get_file_data(first_file)
+    new = get_file_data(second_file)
     return {'stylish': gen_stylish_diff,
             'plain': gen_plain_diff,
             'json': gen_json_diff}[format_type](make_ast(old, new))
